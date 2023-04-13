b3928b66eb4028be20bb77ba60b3b9a09dc6fa4d
Temiloluwa Olubiyi <temiloluwa.olubiyi@yahoo.com>
2023-04-12 22:00:12 -0500

    Create snake initials

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243', // IP address here,
+    port: 50541 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  //.on method - to specify event name
+  // nd a function that does something when that event happens
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+
+  conn.on("connect", () => {
+    conn.write("Successfully connected to game server")
+    conn.write("Name: TAY")
+  })
+
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,22 +1,9 @@
-const net = require("net");
+
+const connect = require('./client');
+
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here,
-    port: 50541 // PORT number here,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  //.on method - to specify event name
-  // nd a function that does something when that event happens
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
