95bc189e897eb904e743465d99099c09c1a5143c
Temiloluwa Olubiyi <temiloluwa.olubiyi@yahoo.com>
2023-04-12 22:44:34 -0500

    Setup stdin event listener and handleUserInput to exit

--- play.js
+++ play.js
@@ -2,8 +2,25 @@
 const connect = require('./client');
 
 
-// establishes a connection with the game server
-
-
 console.log("Connecting ...");
 connect();
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === '\u0003'){
+    process.exit();
+  }
+};
+
+setupInput();
