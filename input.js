386ec064a75b2532b68f8cd5f7ed3e7465430b98
Temiloluwa Olubiyi <temiloluwa.olubiyi@yahoo.com>
2023-04-13 00:24:15 -0500

    Setup Say message to make short chat bubbles  during the game

--- input.js
+++ input.js
@@ -15,26 +15,37 @@ const setupInput = (conn) => {
 };
 
 const handleUserInput = (key) => {
-  if (key === '\u0003'){
-    process.exit();
-  }
-
+// setup WASD movement
   if (key === 'w') {
     connection.write ('Move: up');
   }
-
   if (key === 'a') {
     connection.write ('Move: left');
   }
-
   if (key === 's') {
     connection.write ('Move: down');
   }
-
   if (key === 'd') {
     connection.write ('Move: right');
   }
-};
 
+  //send short chat messages
+  if (key === 'i') {
+    connection.write ('Say: Incoming!!')
+  }
+  if (key === 'k') {
+    connection.write ("Say: I'm the ALL TIME CHAMPION")
+  }
+  if (key === 'l') {
+    connection.write ("Say: You're gonna lose")
+  }
+  if (key === 'o') {
+    connection.write ('Say: Leaving, Bye!!')
+  }
+  //to exit the game
+  if (key === '\u0003'){
+    process.exit();
+  };
+}
 
 module.exports = setupInput;
